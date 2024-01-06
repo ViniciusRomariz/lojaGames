c2fcf879d4bdfbb940cbab81241775098d80f354
Vinicius Romariz <vinicius.romariz@al.infnet.edu.br>
2024-01-05 22:11:25 -0300

    Fazendo o Login/Logout

---
+++ auth.ts
@@ -0,0 +1,35 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+import { z } from 'zod';
+import { authConfig } from './auth.config';
+//-------------------------------------------------------
+import type { Usuario } from './app/lib/domain/usuarios';
+import { getUsuarioPorEmail } from "./app/lib/infra/usuarios";
+//-------------------------------------------------------
+
+export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), senha: z.string().min(6) })
+                    .safeParse(credentials);
+
+                if (parsedCredentials.success) {
+                    const { email, senha } = parsedCredentials.data;
+
+                    const usuario = await getUsuarioPorEmail(email);
+                    if (!usuario) return null;
+
+                    const senhaOk = await bcrypt.compare(senha, usuario.senha);
+                    if (senhaOk) return usuario;
+                }
+
+                console.log('Login inválido');
+                return null;
+            },
+        }),
+    ],
+});
